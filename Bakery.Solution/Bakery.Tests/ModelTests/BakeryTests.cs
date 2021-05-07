3826c73adae6b5e1cd9d524ebd186b1957e61c8e
Brandon-Magofna <magofna68@gmail.com>
2021-05-07 08:30:22 -0700

    instantiate the project

---
+++ Bakery.Solution/Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryNameSpace;
+
+namespace BakeryNameSpaceTests //Change to match Title
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+    //   // any necessary logic to prep for test; instantiating new classes, etc.
+    //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+  }
+}
