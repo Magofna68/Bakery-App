76c498a2a79a36dea31627f7e0e3ff021bd65b51
Brandon-Magofna <magofna68@gmail.com>
2021-05-07 11:13:03 -0700

    fail 4th test

--- Bakery.Solution/Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/BakeryTests.cs
@@ -37,5 +37,15 @@ namespace BakeryNameSpaceTests //Change to match Title
       int totalBreadCost = BakeryNameSpace.BreadCost.GetBreadCost(3);
       Assert.AreEqual(expectedCost, totalBreadCost);
     }
+
+    [TestMethod]
+    public void GetBreadCost_EntireFunctionality_30()
+    {
+      int value;
+      int breadCost = 5;
+      int expectedCost = 30;
+      int totalBreadCost = BakeryNameSpace.BreadCost.GetBreadCost(0);
+      Assert.AreEqual(expectedCost, totalBreadCost);
+    }
   }
 };
